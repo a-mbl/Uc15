7b7e3edcb1b6159bdd9c3de93e055a5b60a1bf88
Ale <aleymblira@gmail.com>
2022-10-26 22:39:03 -0300

    SA2 ER 2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Back_End_ER02.Classes;
+
+PessoaFisica novaPF = new PessoaFisica();
+
+novaPF.nome = "Aley";
+novaPF.cpf = "3510521455";
+
+// Console.WriteLine(novaPF.nome);
+// Console.WriteLine(novaPF.cpf);
+// Console.WriteLine("Bem vindo");
+// concatenação 
+Console.WriteLine("Bem vindo - " + novaPF.nome);
+
+//interpolação
+Console.WriteLine($"Bem vindo: {novaPF.nome} de CPF: {novaPF.cpf}");
+
+
+
