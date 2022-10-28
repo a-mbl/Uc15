301e67783461e55b2968f005cd343e7b40bd2b1e
Ale <aleymblira@gmail.com>
2022-10-27 22:54:37 -0300

    SA2 ER3

--- Program.cs
+++ Program.cs
@@ -4,15 +4,26 @@ PessoaFisica novaPF = new PessoaFisica();
 
 novaPF.nome = "Aley";
 novaPF.cpf = "3510521455";
+novaPF.rendimento = 6600.5f;
 
 // Console.WriteLine(novaPF.nome);
 // Console.WriteLine(novaPF.cpf);
-// Console.WriteLine("Bem vindo");
+// Console.WriteLine("Bem vindo");.
+
 // concatenação 
-Console.WriteLine("Bem vindo - " + novaPF.nome);
+//Console.WriteLine("Bem vindo - " + novaPF.nome);
 
 //interpolação
-Console.WriteLine($"Bem vindo: {novaPF.nome} de CPF: {novaPF.cpf}");
+//Console.WriteLine($"Bem vindo: {novaPF.nome} de CPF: {novaPF.cpf}");
+
+// Pessoa física
+float impostaPagar = novaPF.CalcularImposto (novaPF.rendimento);
+Console.WriteLine($"{impostaPagar:0.00}");
+Console.WriteLine(impostaPagar.ToString("C2"));
+
+// Pessoa juridica
+PessoaJuridica novaPj = new PessoaJuridica();
+Console.WriteLine(novaPj.CalcularImposto(6600.5f));
 
 
 
